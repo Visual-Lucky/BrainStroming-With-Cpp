f248ddc5c3167c76d9cdcb05c5a0dad72c1fe987
Visual-Lucky <luckybhandari122@gmail.com>
2025-07-18 15:35:29 +0530

    HRMS

--- Projects/Human Resource Management System/HRMS_prototype.cpp
+++ Projects/Human Resource Management System/HRMS_prototype.cpp
@@ -7,17 +7,18 @@ using namespace std;
 
 class HR_Admin{
 
+     //file : adminDetails.dat
     //H.R.{Admin} and its Details: 
 
     char adminID[10];// for HR -> HR & User ADMN1 , ADMN2.....
     char adminName[20];
-    char adminPassword[12];
-    char adminContactDetails[15];
     char adminAddress[50];
-
-    //file : AdminDetails
+    char adminPassword[12];
+    char adminContactDetails[11];
 
     public:
+
+    void adminMenu(){}
 
     bool approveHoliday(){}
 
@@ -29,10 +30,18 @@ class HR_Admin{
 
     void assignProject(){}
 
-    
+    void allSalarySlips(){}
+
+    void approveSalaryRelease(){}
+
+    bool Login(){}
 };
 
 class Employee{
+         //file : employeeDetails.dat
+        //file : holiDayRequests.dat
+       //file : salarySlips.dat
+      //file : projectList.dat
 
     char empID[10];
     char empName[20];
@@ -48,19 +57,43 @@ class Employee{
 
     //char empRemarks[20];
 
-
-    //file : EmployeeDetails
+    void employeeMenu(){}
 
     void empHoliday(){}
+
+    void viewProjects(){}
+
+    void generateSlip(){}
+
+    void SubmitWork(){}
+
+    bool Login(){}
 };
 
-class Project_and_Clients{};
+class Project_and_Clients{
 
-class Brands_and_Sales{};
+     //file : clientList.dat
+    //file : projectList.dat
 
-class Holiday_and_SlipsGenerator{
-    //this should be merge in Employee
+    //void clientMenu(){} clients seek their project's progress maybe NOT
+
+    void addProject(){}
+
+    void assignToEmployee(){}
+
+    void listOfProjects(){}
 };
+
+class Brands_and_Sales{
+
+    void addBrand(){}
+
+    void recordSale(){}
+
+    void showBrandStatus(){}
+};
+
+int authenticatorMenu(){}
 
 int main(){
     return 0;
